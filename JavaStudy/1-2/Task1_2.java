16460fbebabb05b98a08ad703d6c5d8c800657fa
hikaru-oss <69238230+hikaru-oss@users.noreply.github.com>
2020-08-09 20:41:45 +0900

    課題提出
    Task1_2.javaの作成

---
+++ JavaStudy/1-2/Task1_2.java
@@ -0,0 +1,25 @@
+public class Task1_2 {
+
+    public static void main(String[] args) {
+
+    String[] country = {"Japan", "america", "Korea"};
+
+
+    System.out.println(country.length);
+
+    String[] strArray = new String[3];
+
+    strArray[0] = "りんご";
+    strArray[1] = "もも";
+    strArray[2] = "ぶどう";
+
+    System.out.println(strArray[1]);
+
+    //10,20,30,40,50の要素の値(初期値)とする配列intArrayの作成
+    int[] intArray = { 10, 20, 30, 40, 50 };
+    
+    //2番目の要素の値と5番目の要素の値の合計の出力
+    System.out.println(intArray[1] + intArray[4]);
+
+    }
+}
