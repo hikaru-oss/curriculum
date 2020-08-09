6a87e6eb90113ef3fd7e733990bba8db58718202
hikaru-oss <69238230+hikaru-oss@users.noreply.github.com>
2020-08-09 20:38:31 +0900

    課題提出
    Task1_１.javanの作成

---
+++ JavaStudy/1-1/Task1_1.java
@@ -0,0 +1,28 @@
+public class Task1_1 {
+
+    public static void main(String[] args) {
+
+       int hogeInt = 50;
+
+       String hogeString = "テスト";
+
+        //double型のhogeDoubleという名前の変数を宣言、値0.2で初期化
+        
+        double hogeDouble = 0.2;
+
+         hogeInt = 100;
+        
+         hogeString = "合格";
+
+        System.out.println(hogeInt);
+
+        //変数hogeStringの値を表示
+
+        System.out.println(hogeString);
+
+        System.out.print(hogeDouble);
+
+
+
+    }
+}
