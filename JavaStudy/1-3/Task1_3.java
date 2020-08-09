f311bd8b4e2cf623c304d7f911c54ca5456c921a
hikaru-oss <69238230+hikaru-oss@users.noreply.github.com>
2020-08-10 00:57:16 +0900

    課題提出
    Task1_3.javaの作成

---
+++ JavaStudy/1-3/Task1_3.java
@@ -0,0 +1,29 @@
+public class Task1_3 {
+
+    public static void main(String[] args) {
+
+    int plus = 0;
+    plus = 5 + 10;
+    System.out.println(plus);
+
+    int minus = 0;
+    minus = 20 - 7;
+    System.out.println(minus);
+
+    
+    //10と2を乗算した値を代入し、変数multiplyを作成
+    int multiply  = 10 * 2;
+    System.out.println(multiply);
+   
+    
+    int remi = 20 % 6;
+    System.out.println(remi);
+   
+    //Stringクラスを宣言して、変数名townで有楽町で初期化し、
+    //変数名lineで線で初期化する。
+    String town = "有楽町";
+    String line = "線";
+    String train = town + line;
+    System.out.println(train);
+   }
+}
